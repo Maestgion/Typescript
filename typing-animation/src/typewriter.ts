8523248391feb00846f1f10105cd905bac92a34c
Abhishek Dubey <7abhishek1410@gmail.com>
2023-06-14 00:01:03 +0530

    all-supporting-actions-completed

--- typing-animation/src/typewriter.ts
+++ typing-animation/src/typewriter.ts
@@ -17,5 +17,81 @@ export default class typewriter{
         this.#typingSpeed = typingSpeed
         this.#loop = loop
     }
+
+    // displaying current string on the screen
+    typeSentence(string:string)
+    {
+        this.#addAction(resolve=>{
+            let i = 0;
+            const intervalId = setInterval(()=>{
+                this.#element.innerText += string[i]
+
+                if(i>=string.length)
+                {
+                    clearInterval(intervalId)
+                    resolve()
+                }
+            }, this.#typingSpeed)
+        })
+    }
+
+    // deleting characters 
+
+    deleteChars(number: number)
+    {
+        this.#addAction(resolve=>{
+            let i=0
+            const intervalId = setInterval(()=>{
+                this.#element.innerText = this.#element.innerText.slice(0, -1)
+
+                if(i>=number)
+                {
+                    clearInterval(intervalId)
+                    resolve()
+
+                }
+            }, this.#deletingSpeed)
+        })
+    }
+
+    // deleting whole senetence
+    deleteAll()
+    {
+        this.#addAction(resolve=>{
+            const intervalId = setInterval(()=>{
+                this.#element.innerText = this.#element.innerText.slice(0, -1)
+                if(this.#element.innerText.length===0)
+                {
+                    clearInterval(intervalId)
+                    resolve()
+                }
+            }, this.#deletingSpeed)
+        })
+    }
+
+    // pause
+    pauseFor(duration: number)
+    {
+        this.#addAction(resolve=>{
+            setTimeout(resolve, duration)
+        })
+    }
+
+    // start
+    start()
+    {
+        
+    }
+
+    // adding action to the actions array by using a callback function cb which returns nothing
+
+    #addAction(cb: (resolve:()=>void)=>void) 
+    {
+        this.#action.push(()=>new Promise(cb))
+    }
+
+
+
+
 }
 
